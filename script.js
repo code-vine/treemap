2c0ed99260564af06e750cc8dfcf254e68b4767f
code-vine <caldwell.vincent@outlook.com>
2023-04-06 10:16:26 -0500

    refactors code, creates d3 hierarchy

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const movieDataUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json';
 
-let movieData
-
+let width = 1000;
+let height = 600;
 const svg = d3.select("svg")
-              .attr("width", 1000)
-              .attr("height", 600);
+              .attr("width", width)
+              .attr("height", height);
 
 d3.json(movieDataUrl)
   .then((data)=>{
-    movieData = data;
+
+    let hierarchy = d3.hierarchy(data, (node)=>{
+        return node.children;
+    }).sum((node =>{
+        return node.value;
+    })).sort((node1,node2)=>{
+        return node2.value - node1.value;
+    })
+
+    
+
   })
   .catch((error) => console.log(error));
